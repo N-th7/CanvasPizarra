fd6bf8bf542d47ef9e2f735855bf0240f59b0ec4
N-th7 <64654131+N-th7@users.noreply.github.com>
2022-10-18 23:16:21 -0400

    imagenes agregadas

--- src/components/botonesGrosor/BotonesGrosor.jsx
+++ src/components/botonesGrosor/BotonesGrosor.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-
+import grosor from "../imagenes/grosor.png";
+import grosor2 from "../imagenes/grosor2.png"
+import grosor3 from "../imagenes/grosor3.png"
+import grosor4 from "../imagenes/grosor4.png"
 const BotonesGrosor = () => {
     return(
         <div class="grosor">
                     <h1>Grosor</h1>
-                    <button class="boton" onclick="cambiarWidth('20')"><img src="../imagenes/grosor.png"/></button><br/>
-                    <button class="boton" onclick="cambiarWidth('10')"><img src="../imagenes/grosor2.png"/></button><br/>
-                    <button class="boton" onclick="cambiarWidth('8')"><img src="../imagenes/grosor3.png"/></button><br/>
-                    <button class="boton" onclick="cambiarWidth('3')"><img src="../imagenes/grosor4.png"/></button>
+                    <button class="boton" onclick="cambiarWidth('20')"><img src={grosor}/></button><br/>
+                    <button class="boton" onclick="cambiarWidth('10')"><img src={grosor2}/></button><br/>
+                    <button class="boton" onclick="cambiarWidth('8')"><img src={grosor3}/></button><br/>
+                    <button class="boton" onclick="cambiarWidth('3')"><img src={grosor4}/></button>
         </div>
     );
 }
