db3a5452d6b96c83bad73e7126c3b7524eec06fd
201900531 <201900531@est.umss.edu>
2022-10-26 01:00:53 -0400

    Insertar imagen react

--- src/components/botonesGrosor/BotonesGrosor.jsx
+++ src/components/botonesGrosor/BotonesGrosor.jsx
@@ -3,18 +3,18 @@ import grosor1 from "../imagenes/grosor.png";
 import grosor2 from "../imagenes/grosor2.png"
 import grosor3 from "../imagenes/grosor3.png"
 import grosor4 from "../imagenes/grosor4.png"
-const BotonesGrosor = ({grosor}) => {
+const BotonesGrosor = ({grosor, setGrosor}) => {
 
     function cambiarWidth(width){
-        grosor=width;
+        setGrosor(width);
     }
     return(
         <div className="grosor">
                     <h1>Grosor</h1>
-                    <button className="boton" onClick={cambiarWidth('20')}><img src={grosor1}/></button><br/>
-                   {/* <button className="boton" onClick="cambiarWidth('10')"><img src={grosor2}/></button><br/>
-                    <button className="boton" onClick="cambiarWidth('8')"><img src={grosor3}/></button><br/>
-    <button className="boton" onClick="cambiarWidth('3')"><img src={grosor4}/></button>*/}
+                    <button className="boton" onClick={() => cambiarWidth(20)}><img src={grosor1}/></button><br/>
+                    <button className="boton" onClick={() => cambiarWidth(10)}><img src={grosor2}/></button><br/>
+                    <button className="boton" onClick={() => cambiarWidth(8)}><img src={grosor3}/></button><br/>
+                    <button className="boton" onClick={() => cambiarWidth(3)}><img src={grosor4}/></button>
         </div>
     );
 }
