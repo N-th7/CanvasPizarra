0c3e6adf504011b026f8e53ce5f3dd0ea1b504e1
201900531 <201900531@est.umss.edu>
2022-11-11 01:51:57 -0400

    WebStorage

--- src/App.js
+++ src/App.js
@@ -1,217 +1,259 @@
-import Lienzo from './components/lienzo/Lienzo';
-import Menu from './components/menu/Menu';
-import './App.css';
-import React, { useEffect, useRef, useState } from 'react';
+import Lienzo from "./components/lienzo/Lienzo";
+import Menu from "./components/menu/Menu";
+import "./App.css";
+import React, { useEffect, useRef, useState } from "react";
+import { Basurero } from "./components/Basurero";
 
 function App() {
-  const [width, setGrosor] = useState(12)
-  const [color, setColor] = useState("#000")
-  const [drawID, setDrawID] = useState("")
+  const [width, setGrosor] = useState(12);
+  const [color, setColor] = useState("#000");
+  const [drawID, setDrawID] = useState("");
 
-  let rect =false;
-  let line= false;
-  let circle= false;
+  let rect = false;
+  let line = false;
+  let circle = false;
   let initialX;
-    let initialY;
-   console.log(width);
-   let inix;
-   let iniy;
-   let finx;
-   let finy;
-   let figura=false;
-   const [texto, setTexto] = useState("")
-   
-  useEffect(()=>{
+  let initialY;
+  console.log(width);
+  let inix;
+  let iniy;
+  let finx;
+  let finy;
+  let figura = false;
+  const [texto, setTexto] = useState("");
+
+  useEffect(() => {
     const mainCanvas = document.getElementById("main-canvas");
     const context = mainCanvas.getContext("2d");
     context.lineWidth = width;
     context.strokeStyle = color;
 
     const $canvas = document.querySelector("#main-canvas"),
-$btnDescargar = document.querySelector("#btnDescargar");
+      $btnDescargar = document.querySelector("#btnDescargar");
 
-$btnDescargar.addEventListener("click", () => {
-  // Crear un elemento <a>
-  let enlace = document.createElement('a');
-  // El título
-  enlace.download = "Canvas como imagen.png";
-  // Convertir la imagen a Base64 y ponerlo en el enlace
-  enlace.href = $canvas.toDataURL();
-  // Hacer click en él
-  enlace.click();
-});     
-      const dibujar = (cursorX, cursorY, evt) => {
-        if(figura){
-          context.strokeStyle= '#ffffff00'; // El mismo que antes, blanco con 50% de transparencia.
-          console.log("hollllllllaa")
-        }
+    $btnDescargar.addEventListener("click", () => {
+      // Crear un elemento <a>
+      let enlace = document.createElement("a");
+      // El título
+      enlace.download = "Canvas como imagen.png";
+      // Convertir la imagen a Base64 y ponerlo en el enlace
+      enlace.href = $canvas.toDataURL();
+      // Hacer click en él
+      enlace.click();
+    });
+    const dibujar = (cursorX, cursorY, evt) => {
+      if (figura) {
+        context.strokeStyle = "#ffffff00"; // El mismo que antes, blanco con 50% de transparencia.
+        console.log("hollllllllaa");
+      }
       context.beginPath();
       context.moveTo(initialX, initialY);
-      
+
       context.lineCap = "round";
       context.lineJoin = "round";
       context.lineTo(cursorX, cursorY);
       context.stroke();
-    
+
       initialX = cursorX;
       initialY = cursorY;
     };
 
-    const mouseDown = (evt) => { 
+    const mouseDown = (evt) => {
       initialX = evt.offsetX;
       initialY = evt.offsetY;
       dibujar(initialX, initialY);
-        console.log(initialX+ " primero " + initialY);
-        inix=initialX;
-        iniy=initialY;
-    
+      console.log(initialX + " primero " + initialY);
+      inix = initialX;
+      iniy = initialY;
+
       mainCanvas.addEventListener("mousemove", mouseMoving);
     };
-    
+
     const mouseMoving = (evt) => {
       dibujar(evt.offsetX, evt.offsetY);
     };
-    
-    const mouseUp = (evt) => {
-      finx=evt.offsetX;
-      finy= evt.offsetY;
-      console.log(finx +"     " + finy);
-      if(rect){
-        context.strokeStyle=color
-        context.strokeRect(inix, iniy, finx-inix,finy-iniy);
-        rect=false
-          console.log(rect);
-          figura=false
-      }
-      if(line){
-        context.strokeStyle=color
-        context.moveTo(inix,iniy)
-        context.lineTo(finx,finy)
-        context.stroke()
-        line=false
-        figura=false
-      }
-      if(circle){
-        context.strokeStyle=color
-        context.beginPath()
-        context.arc(inix,iniy,Math.sqrt((finx-inix)*(finx-inix)+(finy-iniy)*(finy-iniy)), 0, 2*Math.PI)
-        context.stroke();
-        circle=false
-        figura=false
 
+    const mouseUp = (evt) => {
+      finx = evt.offsetX;
+      finy = evt.offsetY;
+      console.log(finx + "     " + finy);
+      if (rect) {
+        context.strokeStyle = color;
+        context.strokeRect(inix, iniy, finx - inix, finy - iniy);
+        rect = false;
+        console.log(rect);
+        figura = false;
+      }
+      if (line) {
+        context.strokeStyle = color;
+        context.moveTo(inix, iniy);
+        context.lineTo(finx, finy);
+        context.stroke();
+        line = false;
+        figura = false;
+      }
+      if (circle) {
+        context.strokeStyle = color;
+        context.beginPath();
+        context.arc(
+          inix,
+          iniy,
+          Math.sqrt(
+            (finx - inix) * (finx - inix) + (finy - iniy) * (finy - iniy)
+          ),
+          0,
+          2 * Math.PI
+        );
+        context.stroke();
+        circle = false;
+        figura = false;
       }
 
       mainCanvas.removeEventListener("mousemove", mouseMoving);
     };
 
-
-    
     mainCanvas.addEventListener("mousedown", mouseDown);
     mainCanvas.addEventListener("mouseup", mouseUp);
-    
   });
   const changeFig = (fig) => {
-    figura=true
-    console.log(fig)
-    if(fig=='rect'){
-      rect=true;
-    }else{
-      if(fig=='line'){
-        line=true;
-        console.log(line)
-      }else{
-          circle=true;
-        }
+    figura = true;
+    console.log(fig);
+    if (fig == "rect") {
+      rect = true;
+    } else {
+      if (fig == "line") {
+        line = true;
+        console.log(line);
+      } else {
+        circle = true;
       }
     }
-
-  
+  };
 
   const agregarTexto = () => {
     var canvas = document.getElementById("main-canvas");
     var ctx = canvas.getContext("2d");
     ctx.font = "bold 22px sans-serif";
-    ctx.fillText(texto,90,90);
-  }
+    ctx.fillText(texto, 90, 90);
+  };
 
   const limpiar = () => {
     const mainCanvas = document.getElementById("main-canvas");
     const context = mainCanvas.getContext("2d");
     context.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
-}
-const onImageChange = (e) => {
-  var canvas = document.getElementById("main-canvas");
-  var ctx = canvas.getContext("2d");
-  var img = new Image();
-  img.src="https://www.purina.es/sites/default/files/2021-02/BREED%20Hero_0034_chihuahua_smooth.jpg";
+  };
+  const onImageChange = (e) => {
+    var canvas = document.getElementById("main-canvas");
+    var ctx = canvas.getContext("2d");
+    var img = new Image();
+    img.src =
+      "https://www.purina.es/sites/default/files/2021-02/BREED%20Hero_0034_chihuahua_smooth.jpg";
 
-  ctx.drawImage(img, finx, finy);
-  console.log(img)
+    ctx.drawImage(img, 0, 0);
+    console.log(img);
+  };
 
-
-
-}
-
-  const micanvas = useRef(null)
-
+  const micanvas = useRef(null);
 
   const guardarDibujo = () => {
-    if(!drawID){
-      alert("La imagen no tiene nombre!!")
+    if (!drawID) {
+      alert("La imagen no tiene nombre!!");
       return;
     }
-    const canvas = document.querySelector("#main-canvas")
-    const imagen = canvas.toDataURL()
-    localStorage.setItem(drawID, imagen)
-    alert("Imagen guardada")
-  } 
+    const canvas = document.querySelector("#main-canvas");
+    const imagen = canvas.toDataURL();
+    localStorage.setItem(drawID, imagen);
+    alert("Imagen guardada");
+    window.location.reload()
+  };
   const enlistar = () => {
-    let lista = []
-    for (let x = 0; x <= localStorage.length-1; x++)  {  
-      let clave = localStorage.key(x);  
-      lista.push(clave)
+    let lista = [];
+    for (let x = 0; x <= localStorage.length - 1; x++) {
+      let clave = localStorage.key(x);
+      lista.push(clave);
     }
     return lista;
-  }
+  };
 
   const actualizar = (imgID) => {
     let canvas = document.getElementById("main-canvas");
     let ctx = canvas.getContext("2d");
     let image = new Image();
-    image.onload = function() {
-    ctx.drawImage(image, 0, 0);
+    image.onload = function () {
+      ctx.drawImage(image, 0, 0);
     };
     image.src = localStorage.getItem(imgID);
     setDrawID(imgID);
+  };
+
+  const borrarDibujo = (imgID) => {
+    localStorage.removeItem(imgID)
+    alert("Dibujo eliminado")
+    window.location.reload()
   }
 
+  const borrador = () => {
+    setColor("#FFF")
+  }
   return (
     <div className="App">
-        <div className="w3-row">
-          <ul>
-          {enlistar().map( (item, index) => <li key={index}><button onClick={() => actualizar(item)}>{item}</button></li>)}
-          </ul>
+      <div className="w3-row" style={{display:"flex", flexDirection:"row"}}>
+        <ul style={{backgroundColor:"#A7A59A", padding:"2%", margin:"20px",  borderRadius:"10px", width:"20%"}}>
+        <h1>Menú</h1>
+          {enlistar().map((item, index) => (
+            <li key={index}>
+              <button type="button" onClick={() => actualizar(item)} style={{marginRight:"10px"}}>{item}</button>
+              <button type="button" onClick={() => borrarDibujo(item)}>
+                <Basurero />
+              </button>
+            </li>
+          ))}
+            <button id="btnDescargar">Descargar</button>
+        </ul>
         <div className="w3-col l8 w3-center board">
-            <main className="main-container">
-                <canvas id="main-canvas" width="950" height="650"></canvas>
-            </main>        
+          <main className="main-container">
+            <canvas id="main-canvas" width="950" height="650"></canvas>
+          </main>
         </div>
-          <div className="w3-col l4 w3-center">
-                <div className="opciones w3-center">
-                <Menu setColor={setColor} setGrosor={setGrosor} changeFig={changeFig} texto={texto} setTexto={setTexto} agregarTexto={agregarTexto}></Menu>
-                            <button className="limpiar" id="reset" onClick={limpiar}>Limpiar</button><br/>
-                            <input className="subir" id="imagen" type="file" accept="image/*" multiple  onChange={onImageChange} /><br/>
-                            <input type="text" value={drawID} onChange={(e) => setDrawID(e.target.value)}/>
-                            <button type='button' onClick={guardarDibujo}>Guardar Dibujo</button><br/>
-                            <button id="btnDescargar">Descargar</button>
-                </div>
-
+        <div className="w3-col l4 w3-center">
+          <div className="opciones w3-center">
+            <Menu
+              setColor={setColor}
+              setGrosor={setGrosor}
+              changeFig={changeFig}
+              texto={texto}
+              setTexto={setTexto}
+              agregarTexto={agregarTexto}
+            ></Menu>
+            <button className="limpiar" id="reset" onClick={borrador}>
+              Borrador
+            </button>
+            <button className="limpiar" id="reset" onClick={limpiar}>
+              Limpiar
+            </button>
+            <br />
+            <input
+              className="subir"
+              id="imagen"
+              type="file"
+              accept="image/*"
+              multiple
+              onChange={onImageChange}
+            />
+            <br />
+            <input
+              type="text"
+              value={drawID}
+              onChange={(e) => setDrawID(e.target.value)}
+            />
+            <button type="button" onClick={guardarDibujo}>
+              Guardar Dibujo
+            </button>
           </div>
         </div>
+      </div>
     </div>
   );
 }
-
-
 
 export default App;
