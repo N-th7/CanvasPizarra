da18c1d675e6ca4b69db94eccb859cddb95cf9d0
N-th7 <64654131+N-th7@users.noreply.github.com>
2022-11-29 19:29:02 -0400

    implementacion de rutas

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import Main from "./pages/Main";
 import UsuarioPropietario from "./pages/UsuarioPropetario";
 import UsuarioInvitado from "./pages/UsuarioInvitado";
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
  
   return (
-   <UsuarioInvitado></UsuarioInvitado>
+   <BrowserRouter>
+    <Routes>
+      <Route exact path="/propietario" element={<UsuarioPropietario />}> </Route>
+      <Route exact path="/invitado" element={<UsuarioInvitado />}> </Route>
+      <Route exact path="/main" element={<Main />}> </Route>
+    </Routes>
+   </BrowserRouter>
   );
 }
 
